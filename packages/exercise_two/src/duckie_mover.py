6ec580881eca5295048c17d0435fed7c15e9e50c
fahimfss <fshahri1@ualberta.ca>
2023-02-03 16:34:02 -0700

    Straight mover

--- packages/exercise_two/src/duckie_mover.py
+++ packages/exercise_two/src/duckie_mover.py
@@ -37,33 +37,41 @@ class DuckieMover(DTROS):
         self.lt_initial_val = 0
         self.lt_dist = 0
 
+        self.initial_diff = 0
+
     def go_straight_dist(self, vel, dist):
         msg_velocity = Twist2DStamped()
-
         rate = rospy.Rate(20)
-
         dist_cover = 0
+        initial_wheel_diff = 0
+        wheel_diff_set = False
 
-        while dist_cover < dist:
+        while dist_cover <  abs(dist):
             msg_velocity.header.stamp = rospy.Time.now()
             msg_velocity.v = vel
-            msg_velocity.omega = 0.05
+            omega_offset = 0
+
+            if self.lt_initial_val != 0 and self.rt_initial_val != 0:
+                if not wheel_diff_set:
+                    wheel_diff_set = True
+                    initial_wheel_diff = self.lt_initial_val - self.rt_initial_val
+
+                wheel_diff = self.lt_val - self.rt_val
+
+                if wheel_diff > initial_wheel_diff:
+                    omega_offset = wheel_diff / 7500
+                else:
+                    omega_offset = -wheel_diff / 7500
+
+            msg_velocity.omega = omega_offset
+            
+            self.vel_pub.publish(msg_velocity)
             rate.sleep()
-            dist_cover = (self.lt_dist + self.rt_dist)/2
+            dist_cover = abs((self.lt_dist + self.rt_dist)/2)
+            # rospy.loginfo("Distance covered: " + str(dist_cover))
 
         rospy.loginfo("Distance covered: " + str(dist_cover))
 
-
-    def go_straight(self, vel, duration):
-        msg_velocity = Twist2DStamped()
-        msg_velocity.header.stamp = rospy.Time.now()
-        msg_velocity.v = vel
-        msg_velocity.omega = 0.05
-        self.vel_pub.publish(msg_velocity)
-        dist_cover = (self.lt_dist + self.rt_dist)/2
-        rospy.loginfo("Sending v: " + str(vel) + ", omega = 0")
-        time.sleep(duration)
-        rospy.loginfo("Distance covered: " + str(dist_cover))
 
     def stop(self, duration):
         msg_velocity = Twist2DStamped()
@@ -86,17 +94,21 @@ class DuckieMover(DTROS):
         time.sleep(duration)
 
     def right_tick(self, msg):
+        # rospy.loginfo(msg.data)
+        tk = msg.data + 99999999
         if self.rt_val == 0:
-            self.rt_initial_val = msg.data
-        self.rt_val = msg.data
+            self.rt_initial_val = tk
+        self.rt_val = tk
         tick_diff = self.rt_val - self.rt_initial_val
         self.rt_dist = (2 * pi * self.r * tick_diff) / 135
 
 
     def left_tick(self, msg):
+        # rospy.loginfo(msg.data)
+        tk = msg.data + 99999999
         if self.lt_val == 0:
-            self.lt_initial_val = msg.data
-        self.lt_val = msg.data
+            self.lt_initial_val = tk
+        self.lt_val = tk
         tick_diff = self.lt_val - self.lt_initial_val
         self.lt_dist = (2 * pi * self.r * tick_diff) / 135
         
@@ -104,15 +116,18 @@ class DuckieMover(DTROS):
     def run(self):
         rate = rospy.Rate(10)
 
-        while self.stop_pub.get_num_connections() < 1:
-            rate.sleep()
-        while self.vel_pub.get_num_connections() < 1:
-            rate.sleep()
+        # while self.stop_pub.get_num_connections() < 1:
+        #     rate.sleep()
+        # while self.vel_pub.get_num_connections() < 1:
+        #     rate.sleep()
 
         rospy.loginfo("Start...")
 
         self.go_straight_dist(0.5, 1.25)
         self.reset()
+        for i in range(10):
+            self.stop(0.1)
+
         self.go_straight_dist(-0.5, 1.25)
         
         for i in range(10):
